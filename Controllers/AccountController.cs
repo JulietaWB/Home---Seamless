c8ed2b37ca07001b9643fb176d3a9ba82083e823
JULIETA WAINHAUS BARBER <48460067@ort>
2024-11-07 15:12:07 -0300

    añadí el login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP9LoginJS.Models;
+
+namespace TP9LoginJS.Controllers;
+
+public class Account : Controller
+{
+    [HttpPost]
+    public IActionResult ValidarUsuario (string email, string contra)
+    {
+        if (BD.ValidarUsuarioBD (email, contra) == null)
+        {
+            
+            return RedirectToAction("Login", "Home", new {error = "Correo o contraseña incorrectos"});
+        }
+        else 
+        {
+            return RedirectToAction("PagBienvenida", "Home");
+        }
+    }
+
+    public IActionResult RecuperarContraseña (string email)
+    {  
+        ViewBag.Contra = BD.EncontrarContra(email);
+        if (ViewBag.Contra == null)
+        {
+            return RedirectToAction("Olvide", "Home",new {error = "Usuario no encontrado."});
+        }
+
+        return RedirectToAction("Olvide", "Home", new {contra=ViewBag.Contra});
+    }
+
+    public IActionResult GuardarUsuario (string username, string email, string tel, string name, string contra, string contra2)
+    {  
+        if (BD.BuscarUser(username) != null)
+        {
+            return RedirectToAction("Registro", "Home",new {error = "Ya existe un usuario con ese Username."});
+        }
+        else if (BD.BuscarCorreo(email) != null)
+        {
+            return RedirectToAction("Registro", "Home",new {error = "Ya existe un usuario con ese correo electrónico."});
+        }
+        else
+        {
+            BD.CrearUsuario(username, email, tel, name, contra);
+            return RedirectToAction("Registro", "Home",new {error = "Usuario creado exitosamente."});  
+        }
+    }
+
+}
---
+++ Models/BD.cs
@@ -0,0 +1,62 @@
+using System.Data.SqlClient;
+using Dapper;
+namespace TP9LoginJS.Models;
+
+public static class BD
+{
+    private static string _ConnectionString = "Server=localhost;DataBase=BDtp9Login;Trusted_Connection=true;";
+    public static string ValidarUsuarioBD(string correo, string contra)
+    {
+        string usuario = null;
+        using(SqlConnection conn = new SqlConnection(_ConnectionString))
+        {
+            string sql="SELECT Correo from Usuarios where Correo=@pcorreo and Contraseña=@pcontra";
+            usuario = conn.QueryFirstOrDefault<string>(sql, new {pcontra = contra, pcorreo = correo});
+        }
+        return usuario;
+    }
+
+    public static string EncontrarContra(string correo)
+    {
+        string contra = null;
+        using(SqlConnection conn = new SqlConnection(_ConnectionString))
+        {
+            string sql="SELECT Contraseña from Usuarios where Correo=@pcorreo";
+            contra = conn.QueryFirstOrDefault<string>(sql, new {pcorreo = correo});
+        }
+        return contra;
+    }
+
+    public static string BuscarUser (string user)
+    {
+        string existe = null;
+        using(SqlConnection conn = new SqlConnection(_ConnectionString))
+        {
+            string sql="SELECT Username from Usuarios where Username=@pUser";
+            existe = conn.QueryFirstOrDefault<string>(sql, new {puser = user});
+        }
+        return existe;
+    }
+
+    public static string BuscarCorreo (string correo)
+    {
+        string existe = null;
+        using(SqlConnection conn = new SqlConnection(_ConnectionString))
+        {
+            string sql="SELECT Correo from Usuarios where Correo=@pcorreo";
+            existe = conn.QueryFirstOrDefault<string>(sql, new {pcorreo = correo});
+        }
+        return existe;
+    }
+
+    public static void CrearUsuario (string username, string email, string tel, string name, string contra)   
+    {
+        using(SqlConnection conn = new SqlConnection(_ConnectionString))
+        {
+            string sql="INSERT INTO Usuarios (Username, Contraseña, Correo, Telefono, Nombre) VALUES (@pusername, @pcontra, @pemail, @ptel, @pname)";
+            conn.Execute(sql, new {pusername=username, pcontra=contra, pemail = email, ptel=tel, pname=name});
+        }
+    }
+
+
+}
